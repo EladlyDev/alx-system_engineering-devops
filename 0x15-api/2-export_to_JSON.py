26202542bd7e001a0b1ede3ee789e1499c479e52
Mohamed Eladly <msrtv365@yahoo.com>
2024-03-25 11:04:28 +0200

    2. Export to JSON

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,1 +1,31 @@
 #!/usr/bin/python3
+""" for a given employee ID, returns information
+about his/her TODO list progress. from api """
+import json
+import requests
+from sys import argv
+
+
+if __name__ == '__main__':
+    users = requests.get('https://jsonplaceholder.typicode.com/users').json()
+    prog = requests.get('https://jsonplaceholder.typicode.com/todos').json()
+    tasks = [task for task in prog if str(task['userId']) == argv[1]]
+    done = [task for task in tasks if task['completed']]
+    out = '{}.json'.format(argv[1])
+
+    name = ''
+    username = ''
+    for user in users:
+        if argv[1] == str(user['id']):
+            name = user['name']
+            username = user['username']
+            break
+
+    data = {argv[1]: []}
+    for task in tasks:
+        data[argv[1]].append({'task': task['title'],
+                              'completed': task['completed'],
+                              'username': username})
+
+    with open(out, 'w') as f:
+        f.write(json.dumps(data))
