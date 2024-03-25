df040781e4e629538ff4c39d4a3bdf1fa27c2734
Mohamed Eladly <msrtv365@yahoo.com>
2024-03-25 11:18:59 +0200

    3. Dictionary of list of dictionaries

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,1 +1,27 @@
 #!/usr/bin/python3
+""" for a given employee ID, returns information
+about his/her TODO list progress. from api """
+import json
+import requests
+from sys import argv
+
+
+if __name__ == '__main__':
+    users = requests.get('https://jsonplaceholder.typicode.com/users').json()
+    prog = requests.get('https://jsonplaceholder.typicode.com/todos').json()
+    out = 'todo_all_employees.json'
+
+    all_data = {}
+    for user in users:
+        data = {user['id']: []}
+        tasks = [task for task in prog if task['userId'] == user['id']]
+        done = [task for task in tasks if task['completed']]
+        username = user['username']
+        for task in tasks:
+            data[user['id']].append({'username': username,
+                                     'task': task['title'],
+                                     'completed': task['completed']})
+        all_data.update(data)
+
+    with open(out, 'w') as f:
+        f.write(json.dumps(all_data))
