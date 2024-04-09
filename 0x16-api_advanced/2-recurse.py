26360c4570e80644729fffce1324672c3ef04c33
Mohamed Eladly <msrtv365@yahoo.com>
2024-04-09 22:46:49 +0200

    2. Recurse it!

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,1 +1,26 @@
 #!/usr/bin/python3
+""" 2. Recurse it! """
+import requests
+
+
+def recurse(subreddit, hot_list=[], next=''):
+    """ prints the titles of all the hot posts listed
+    for a given subreddit. (recursive) """
+    url = 'https://www.reddit.com/r/{}/hot.json?after={}'.format(subreddit,
+                                                                 next)
+    headers = {"User-Agent": "ubuntu:Python"}
+
+    r = requests.get(url, headers=headers, allow_redirects=False)
+
+    if not r.ok:
+        return None
+
+    try:
+        top = r.json().get('data')
+        hot_list.append(top.get('children')[0].get('data').get('title'))
+        next = top.get('after')
+        if next:
+            recurse(subreddit, hot_list, next)
+        return hot_list
+    except Exception:
+        return None
