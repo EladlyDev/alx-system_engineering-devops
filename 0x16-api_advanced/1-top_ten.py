de050f7741be9d281ae9ac424b07313c8818c1ba
Mohamed Eladly <msrtv365@yahoo.com>
2024-04-09 22:19:05 +0200

    1. Top Ten

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+""" 1. Top Ten """
+import requests
+
+
+def top_ten(subreddit):
+    """ prints the titles of the first 10 hot posts listed
+    for a given subreddit. """
+    url = 'https://www.reddit.com/r/{}/hot.json?limit=9'.format(subreddit)
+    headers = {"User-Agent": "ubuntu:Python"}
+
+    r = requests.get(url, headers=headers, allow_redirects=False)
+
+    top = None
+    try:
+        top = r.json().get('data').get('children')
+        for post in top:
+            print(post.get('data').get('title'))
+    except Exception as e:
+        print(None)
